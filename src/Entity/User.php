078d4a9777b53565050bf0a29a5865e82086fe2e
gabs <gab.blond@hotmail.com>
2024-06-16 21:41:10 +0200

    Add entity Event

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -33,6 +35,24 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
+
+    /**
+     * @var Collection<int, Event>
+     */
+    #[ORM\OneToMany(targetEntity: Event::class, mappedBy: 'creator', orphanRemoval: true)]
+    private Collection $createdEvents;
+
+    /**
+     * @var Collection<int, Event>
+     */
+    #[ORM\ManyToMany(targetEntity: Event::class, mappedBy: 'participants')]
+    private Collection $joindedEvents;
+
+    public function __construct()
+    {
+        $this->createdEvents = new ArrayCollection();
+        $this->joindedEvents = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -112,5 +132,62 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
     public function getUserIdentifier(): string
     {
         return (string) $this->email;
+    }
+
+    /**
+     * @return Collection<int, Event>
+     */
+    public function getCreatedEvents(): Collection
+    {
+        return $this->createdEvents;
+    }
+
+    public function addCreatedEvent(Event $createdEvent): static
+    {
+        if (!$this->createdEvents->contains($createdEvent)) {
+            $this->createdEvents->add($createdEvent);
+            $createdEvent->setCreator($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCreatedEvent(Event $createdEvent): static
+    {
+        if ($this->createdEvents->removeElement($createdEvent)) {
+            // set the owning side to null (unless already changed)
+            if ($createdEvent->getCreator() === $this) {
+                $createdEvent->setCreator(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Event>
+     */
+    public function getJoindedEvents(): Collection
+    {
+        return $this->joindedEvents;
+    }
+
+    public function addJoindedEvent(Event $joindedEvent): static
+    {
+        if (!$this->joindedEvents->contains($joindedEvent)) {
+            $this->joindedEvents->add($joindedEvent);
+            $joindedEvent->addParticipant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJoindedEvent(Event $joindedEvent): static
+    {
+        if ($this->joindedEvents->removeElement($joindedEvent)) {
+            $joindedEvent->removeParticipant($this);
+        }
+
+        return $this;
     }
 }
