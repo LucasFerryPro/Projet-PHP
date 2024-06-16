a7739d1dd66ebcfefd46ebc76efb23cf5e3a9518
lucas ferry <lucas.ferry.ecole@gmail.com>
2024-06-16 18:54:32 +0200

    make register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,9 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements PasswordAuthenticatedUserInterface, UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -21,6 +25,11 @@ class User
 
     #[ORM\Column(length: 255)]
     private ?string $email = null;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private array $roles = [];
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
@@ -76,5 +85,32 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+        // Method required by the interface, but as plainPassword is not persisted,
+        // no need to do anything here
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
     }
 }
