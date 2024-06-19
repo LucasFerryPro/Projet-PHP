44332445026d0e53caa051aeb2f004cca2dbded4
gabs <gab.blond@hotmail.com>
2024-06-19 12:17:33 +0200

    Add catch for the edit or delete restriction

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Routing\RouterInterface;
+
+class ExceptionListener
+{
+    private $requestStack;
+    private $router;
+
+    public function __construct(RequestStack $requestStack, RouterInterface $router)
+    {
+        $this->requestStack = $requestStack;
+        $this->router = $router;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        $request = $this->requestStack->getCurrentRequest();
+        $session = $request->getSession();
+        if (!$session->isStarted()) {
+            $session->start();
+        }
+        $session->getFlashBag()->add('user_error', $exception->getMessage());
+
+        // Get the event id from the request attributes
+        $eventId = $request->attributes->get('id');
+
+        // Generate the URL for the event show page with the event id
+        $url = $this->router->generate('app_event_show', ['id' => $eventId]);
+
+        $response = new RedirectResponse($url);
+
+        $event->setResponse($response);
+    }
+}
